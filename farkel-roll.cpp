44cac956c075c351e093a1dde20c2206152230e3
Emmett Jaakkola <56277507+Emmennater@users.noreply.github.com>
2024-04-07 01:09:18 -0500

    Update farkel-roll.cpp

--- farkel-roll.cpp
+++ farkel-roll.cpp
@@ -8,37 +8,60 @@ using namespace std;
 int scoreRolls(vector<int>& rolls) {
     int score = 0;
     int occurences[7] = { 0 };
+    
     for (int roll : rolls) {
         ++occurences[roll];
-        
-        if (roll == 1) score += 100;
-        if (roll == 5) score += 50;
     }
 
-    if (occurences[1] == 3) score += 300;
-    if (occurences[2] == 3) score += 200;
-    if (occurences[3] == 3) score += 300;
-    if (occurences[4] == 3) score += 400;
-    if (occurences[5] == 3) score += 500;
-    if (occurences[6] == 3) score += 600;
-
-    bool allOnes = true;
-    int countTwos = 0;
-    int countThrees = 0;
+    // Straight
+    bool isStraight = true;
     for (int i = 1; i <= 6; ++i) {
-        if (occurences[i] != 1) allOnes = false;
-        switch (occurences[i]) {
-            case 2: ++countTwos; break;
-            case 3: ++countThrees; break;
-            case 6: score += 3000; break;
-            case 5: score += 2000; break;
-            case 4: score += 1000; break;
+        if (occurences[i] != 1) {
+            isStraight = false;
+            break;
         }
     }
 
-    if (allOnes) score += 1500;
-    if (countTwos == 3) score += 1500;
-    if (countThrees == 2) score += 2500;
+    if (isStraight) return 1500;
+
+    for (int i = 1; i <= 6; ++i) {
+        // Six of a kind
+        if (occurences[i] == 6) return 3000;
+        
+        // Five of a kind
+        if (occurences[i] == 5) {
+            occurences[i] -= 5;
+            score += 2000;
+        }
+
+        // Four of a kind
+        if (occurences[i] == 4) {
+            occurences[i] -= 4;
+            score += 1000;
+        }
+    }
+
+    // Three pairs or two triplets
+    int twos = 0;
+    int threes = 0;
+    for (int i = 0; i <= 6; ++i) {
+        if (occurences[i] == 2) ++twos;
+        if (occurences[i] == 3) ++threes;
+    }
+
+    if (twos == 3) return 1500;
+    if (threes == 2) return 2500;
+
+    // Three of a kind
+    if (occurences[1] == 3) { occurences[1] -= 3; score += 300; }
+    if (occurences[2] == 3) { occurences[2] -= 3; score += 200; }
+    if (occurences[3] == 3) { occurences[3] -= 3; score += 300; }
+    if (occurences[4] == 3) { occurences[4] -= 3; score += 400; }
+    if (occurences[5] == 3) { occurences[5] -= 3; score += 500; }
+    if (occurences[6] == 3) { occurences[6] -= 3; score += 600; }
+
+    // Individuals
+    score += occurences[1] * 100 + occurences[5] * 50;
 
     return score;
 }
